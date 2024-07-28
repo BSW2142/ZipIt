876ca4438988d43e70e7367a4e2bf6851f1ea599
Westlake <dr.badass2142@gmail.com>
2024-07-27 19:05:07 -0600

    Update zipit.py
    Fixed Comments for .py name change.

--- zipit.py
+++ zipit.py
@@ -5,7 +5,7 @@ Zip It
 Python script for finding folders in a directory, and zipping each of those folders into its own zip file.
 
 Usage:
-	python3 main.py C:/Path/to/folder/
+	python3 zipit.py C:/Path/to/folder/
 
 	If given a path to a folder, it will zip each folder in that directory.
 	Otherwise, it will zip each folder in the current working directory.
